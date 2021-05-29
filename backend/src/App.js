b4f7fbc7f815ea65e0166c27245a99cdafd2668e
Kris Long <corpkris@gmail.com>
2021-05-29 15:37:34 +0200

    Begin building sidebar navigation component

---
+++ backend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SideNav from './components/SideNav';
+
+function App() {
+  return (
+    <div className="App">
+      <SideNav></SideNav>
+    </div>
+  );
+}
+
+export default App;
---
+++ backend/src/components/SideNav.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+export default function SideNav() {
+  const menus = [
+    { to: '/r/popular', text: 'popular' },
+    { to: '/r/all', text: 'all' },
+    { to: '/r/random', text: 'random' },
+  ];
+  const subreddits = [
+    'Funny',
+    'Cats',
+    'Movies',
+    'Watercooler chat',
+    'Art',
+    'Hobbies',
+  ];
+
+  return (
+    <div className="sidenav">
+      <div className="sidenav__logo">
+        <img src="" alt="" />
+      </div>
+      <div className="sidenav__search">
+        <input type="text" name="search" placeholder="search..." />
+      </div>
+      <div className="sidenav__links">
+        <ul className="sidenav__menu">
+          {menus.map((menu) => (
+            <li>
+              <a href={menu.to}>{menu.text}</a>
+            </li>
+          ))}
+        </ul>
+        <hr />
+        <ul className="sidenav__subreddits">
+          {subreddits.map((subreddit) => (
+            <li>
+              <a href={`/r/${subreddit}`}>{subreddit}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
