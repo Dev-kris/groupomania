6c5a9b44e304c65be7c89c8327833a046bfaf3c0
Kris Long <corpkris@gmail.com>
2021-05-31 18:08:54 +0200

    fixed errors (incorrect ` | '  usage)

--- frontend/src/components/postItem.jsx
+++ frontend/src/components/postItem.jsx
@@ -6,20 +6,24 @@ export default function postItem(props) {
   return (
     <div className="post">
       <div className="post__left">
-        <i className="fas fa-caret-up"></i>
+        <button onClick="" className="vote">
+          <i className="fas fa-caret-up"></i>{' '}
+        </button>
         <span>{upvote}</span>
-        <i className="fas fa-caret-down"></i>
+        <button onClick="" className="vote">
+          <i className="fas fa-caret-down"></i>{' '}
+        </button>
       </div>
       <div className="post__center">
-        <img src={image} />
+        <img src={image} alt="User Submitted Content" />
       </div>
       <div className="post__right">
         <h3>
-          <a href={'/r/${subreddit}/${title}'}>{title}</a>
+          <a href={`/r/${subreddit}/${title}`}>{title}</a>
         </h3>
         <span className="post__info">
-          submitted an hour ago by <a href={'/u/${user}'}>{user}</a> to{' '}
-          <a href={'/r/${subreddit}'}>{subreddit}</a>
+          submitted an hour ago by <a href={`/u/${user}`}>{user}</a> to{' '}
+          <a href={`/r/${subreddit}`}>{subreddit}</a>
         </span>
         <p className="post__info">
           {comment_count} comments | share | save | hide | report
